f994790a4aac98a0e5b7c5c51490e05d817195f5
fcj <fcjljy@gmail.com>
2016-01-29 18:05:33 +0800

    add new test for BlogController

---
+++ src/test/java/com/basic/mvc/BlogControllerTest.java
@@ -0,0 +1,134 @@
+package com.basic.mvc;
+
+import com.core.entities.Account;
+import com.core.entities.Blog;
+import com.core.entities.BlogEntry;
+import com.core.services.BlogService;
+import com.core.services.exceptions.BlogNotFoundException;
+import com.core.services.util.BlogEntryList;
+import com.core.services.util.BlogList;
+import com.rest.mvc.BlogController;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by fengc on 1/28/2016.
+ */
+public class BlogControllerTest {
+
+    @InjectMocks
+    private BlogController blogController;
+
+    @Mock
+    private BlogService blogService;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(blogController).build();
+    }
+
+    @Test
+    public void findAllBlogs() throws Exception {
+
+        BlogList foundBlogList = new BlogList();
+        List<Blog> blogs = new ArrayList<Blog>();
+        Blog blogA = new Blog();
+        blogA.setId(1L);
+        blogA.setTitle("Title A");
+        Blog blogB = new Blog();
+        blogB.setId(1L);
+        blogB.setTitle("Title B");
+        blogs.add(blogA);
+        blogs.add(blogB);
+        foundBlogList.setBlogs(blogs);
+
+        when(blogService.findAllBlogs()).thenReturn(foundBlogList);
+        mockMvc.perform(get("/rest/blogs"))
+                .andExpect(jsonPath("$.blogs[*].title", hasItems(endsWith("Title A"), endsWith("Title B"))))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void getBlog() throws Exception {
+        Blog blog = new Blog();
+        blog.setId(1L);
+        blog.setTitle("Test Title");
+
+        Account account = new Account();
+        account.setId(1L);
+        blog.setOwner(account);
+
+        when(blogService.findBlog(1L)).thenReturn(blog);
+        mockMvc.perform(get("/rest/blogs/1"))
+                .andExpect(jsonPath("$.title",is(blog.getTitle())))
+                .andExpect(jsonPath("$.links[*].href", hasItem(endsWith("/rest/blogs/1"))))
+                .andExpect(jsonPath("$.links[*].href",hasItem(endsWith("/rest/blogs/1/entries"))))
+                .andExpect(jsonPath("$.links[*].href",hasItem(endsWith(("/rest/accounts/1")))))
+                .andExpect(jsonPath("$.links[*].rel",hasItems(is("self"),is("entries"),is("owner"))))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
+
+    @Test
+    public void createBlogEntryExistingBlog() throws Exception{
+        BlogEntry createdBlogEntry = new BlogEntry();
+        createdBlogEntry.setId(1L);
+        createdBlogEntry.setTitle("Test Title");
+        Blog blog = new Blog();
+        blog.setId(1L);
+        createdBlogEntry.setBlog(blog);
+
+        when(blogService.createBlogEntry(eq(1L), any(BlogEntry.class))).thenReturn(createdBlogEntry);
+        mockMvc.perform(post("/rest/blogs/1/blog-entries").contentType(MediaType.APPLICATION_JSON).content("{\"title\":\"Generic Title\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title",is(createdBlogEntry.getTitle())))
+                .andExpect(jsonPath("$.links[*].href",hasItem(endsWith("/rest/blog-entries/1"))))
+                .andExpect(jsonPath("$.links[*].href",hasItem(endsWith("/rest/blogs/1"))))
+                .andDo(print());
+    }
+
+
+    @Test
+    public void createBlogEntryNonExistingBlog() throws Exception{
+        when(blogService.createBlogEntry(eq(1L),any(BlogEntry.class))).thenThrow(new BlogNotFoundException());
+        mockMvc.perform(post("/rest/blogs/1/blog-entries").contentType(MediaType.APPLICATION_JSON).content("{\"title\":\"Generic Title\"}"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void findAllBlogEntries() throws Exception {
+        BlogEntryList foundBlogEntryList = new BlogEntryList();
+        List<BlogEntry> lists = new ArrayList<BlogEntry>();
+        foundBlogEntryList.setEntries(lists);
+        when(blogService.findAllBlogEntries(1L)).thenReturn(foundBlogEntryList);
+    }
+
+
+
+}
